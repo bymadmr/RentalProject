95784ed0ab0819ffc4654d62c66a2748b6650105
Muhammed <bymadmr@gmail.com>
2022-02-27 15:58:45 +0300

    12th day homework completed

---
+++ Business/ValidationRules/FluentValidation/BrandValidator.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public  class BrandValidator:AbstractValidator<Brand>
+    {
+        public BrandValidator()
+        {
+            RuleFor(p=>p.Name).NotEmpty().WithMessage("Marka ismi boş olamaz!");
+            RuleFor(p => p.Name).MinimumLength(2);
+            //RuleFor(p => p.Name).Must(StartWithA);
+        }
+
+        //private bool StartWithA(string arg)
+        //{
+        //    return arg.StartsWith("A");
+        //}
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator:AbstractValidator<Rental>
+    {
+        public RentalValidator()
+        {
+            RuleFor(c => c.RentDate).NotEmpty();
+            RuleFor(c => c.CarId).NotEmpty();
+            RuleFor(c => c.CustomerId).NotEmpty();
+        }
+    }
+}
